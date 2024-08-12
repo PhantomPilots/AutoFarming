c1bb7611d53e517dfb39085a4105c944455da94d
PhantomPilot <none>
2024-08-12 19:59:55 -0400

    Going back to DummyBattleStrategy

--- scripts/BirdFarmer.py
+++ scripts/BirdFarmer.py
@@ -47,7 +47,7 @@ def main():
 
     FarmingFactory.main_loop(
         farmer=BirdFarmer,
-        battle_strategy=SmarterBattleStrategy,  # The AI that will pick the cards
+        battle_strategy=DummyBattleStrategy,  # The AI that will pick the cards
         starting_state=States.GOING_TO_BIRD,  # Should be 'GOING_TO_BIRD'
     )
 
