1925ca2e0ce081288ede1de8ca68c31855c2d7a0
PhantomPilot <none>
2024-09-01 09:37:03 -0700

    In phase 4, pick Thor cards if possible, but always leaving one behind to play

--- scripts/utilities/fighting_strategies.py
+++ scripts/utilities/fighting_strategies.py
@@ -276,7 +276,7 @@ class Floor4BattleStrategy(IBattleStrategy):
             print("We can't play any bronze card! Defaulting to arbitrary moving cards")
             # Move cards until a merge of silver cards is NOT generated
             origin_idx = -1
-            target_idx = -3
+            target_idx = -2
             while (
                 determine_card_merge(hand_of_cards[target_idx], hand_of_cards[origin_idx])
                 and card_ranks[origin_idx] == CardRanks.SILVER.value
@@ -317,7 +317,6 @@ class Floor4BattleStrategy(IBattleStrategy):
         if total_num_silver_cards <= 2:
             # If we don't have a shield, move cards to try to make 3 silver cards
             if (potential_idx := self._make_silver_merge(hand_of_cards)) is not None:
-                print("Moving cards:", potential_idx)
                 return potential_idx
 
         # Play level 2 cards or higher only if we can get the shield
@@ -563,12 +562,13 @@ class Floor4BattleStrategy(IBattleStrategy):
             return recovery_ids[-1]
 
         # Go HAM on the fricking bird
+        # TODO: Rather than doing this, ensure that one Thor card is picked last... but most of them first too!
         ham_card_ids = np.where([is_hard_hitting_card(card) for card in hand_of_cards])[0]
         if len(ham_card_ids):
             thor_ids = np.where([is_Thor_card(card) for card in hand_of_cards])[0]
             non_thor_ham_ids = np.setdiff1d(ham_card_ids, thor_ids)
             # Re-order the array of HAM IDs, with the thor_ids in the last position
-            ham_card_ids = np.concatenate([thor_ids, non_thor_ham_ids])
+            ham_card_ids = np.concatenate([thor_ids[-1:], non_thor_ham_ids, thor_ids[:-1]])
             return (
                 thor_ids[-1]
                 if len(thor_ids) and IBattleStrategy.cards_to_play - IBattleStrategy.card_turn == 1
