f2eb08ba021907d9cf6d3cb3dca4dcadc3f42935
PhantomPilot <none>
2024-11-09 11:50:48 +0100

    We need to reset the Deer team after efeat

--- scripts/DeerFarmer.py
+++ scripts/DeerFarmer.py
@@ -9,7 +9,7 @@ def main():
         farmer=DeerFarmer,
         battle_strategy=DeerBattleStrategy,  # The AI that will pick the cards
         starting_state=States.GOING_TO_DB,  # It should be 'GOING_TO_DB'
-        reset_after_defeat=False,  # After we lose, should we reset the Demonic Beast team?
+        reset_after_defeat=True,  # After we lose, should we reset the Demonic Beast team?
         max_stamina_pots="inf",  # How many stamina pots at most
         max_floor_3_clears="inf",  # How many floor 3 clears at most
     )
