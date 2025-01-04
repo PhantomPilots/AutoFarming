9e9e19196ada457eb6666af3997bd8c3e0646914
PhantomPilot <none>
2025-01-04 16:35:18 +0100

    Setting Bell demon as the default demon

--- scripts/DemonFarmer.py
+++ scripts/DemonFarmer.py
@@ -9,8 +9,8 @@ def main():
         farmer=DemonFarmer,
         starting_state=States.GOING_TO_DEMONS,  # Should be 'GOING_TO_DEMONS'
         demons_to_farm=[  # Accepts: vio.red_demon, vio.gray_demon, vio.crimson_demon, vio.bell_demon, vio.og_demon
-            # vio.bell_demon,
-            vio.og_demon,
+            vio.bell_demon,
+            # vio.og_demon,
         ],
         time_to_sleep=9.3,  # How many seconds to sleep before accepting an invitation
         do_dailies=True,  # Do we halt demon farming to do dailies?
