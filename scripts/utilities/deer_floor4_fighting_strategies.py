061b4dbfbf4e3def269d84fdabb1c6863f952330
PhantomPilot <none>
2025-04-08 22:28:39 -0400

    Minor bug fix

--- scripts/utilities/deer_floor4_fighting_strategies.py
+++ scripts/utilities/deer_floor4_fighting_strategies.py
@@ -275,7 +275,7 @@ class DeerFloor4BattleStrategy(IBattleStrategy):
             # Let's pick a card from the color that has the most
             picked_color_ids = max(green_card_ids, red_card_ids, blue_card_ids, key=len)
             idx = -1
-            while find(vio.freyr_ult, hand_of_cards[picked_color_ids[idx]].card_image):
+            while len(picked_color_ids) and find(vio.freyr_ult, hand_of_cards[picked_color_ids[idx]].card_image):
                 print(f"We found Freyr's ult on the picked id {picked_color_ids[idx]}! Saving it for phase 4")
                 idx -= 1
             return picked_color_ids[idx]
