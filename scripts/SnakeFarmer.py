36dba47cc971771a03c9e2dade54f3386dece484
PhantomPilot <none>
2025-04-25 16:36:47 -0400

    Updated demon farmer, and added Indura option (but not fighting logic)

--- scripts/SnakeFarmer.py
+++ scripts/SnakeFarmer.py
@@ -17,7 +17,7 @@ def main():
         farmer=SnakeFarmer,
         battle_strategy=SnakeBattleStrategy,  # The AI that will pick the cards
         starting_state=States.GOING_TO_DB,  # It should be 'GOING_TO_DB'
-        reset_after_defeat=False,  # After we lose, should we reset the Demonic Beast team?
+        reset_after_defeat=True,  # After we lose, should we reset the Demonic Beast team?
         max_stamina_pots="inf",  # How many stamina pots at most
         max_floor_3_clears=args.clears,  # How many floor 3 clears at most
         password=args.password,  # Account password
