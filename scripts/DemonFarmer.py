0cb30e64d5912ce8a259f204802d5ca892f95a47
PhantomPilot <none>
2025-05-27 09:47:27 -0400

    increased default wait time value

--- scripts/DemonFarmer.py
+++ scripts/DemonFarmer.py
@@ -33,7 +33,7 @@ def main():
             vio.indura_demon,
         ],
         indura_difficulty=args.indura_diff,  # Difficulty of Indura demon
-        time_to_sleep=9.15,  # How many seconds to sleep before accepting an invitation
+        time_to_sleep=9.2,  # How many seconds to sleep before accepting an invitation
         time_between_demons=2,  # How many hours between each type of demon
         do_dailies=True,  # Do we halt demon farming to do dailies?
         do_daily_pvp=True,  # If we do dailies, do we do PVP?
