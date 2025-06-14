e36fa57b6091840f5433827c9e7de798886adb4e
PhantomPilot <none>
2025-06-14 13:36:34 +0200

    Added argparse to finalBossfarmer

--- scripts/FinalBossFarmer.py
+++ scripts/FinalBossFarmer.py
@@ -1,14 +1,33 @@
+import argparse
+
 from utilities.farming_factory import FarmingFactory
 from utilities.final_boss_farming_logic import FinalBossFarmer, States
 
 
 def main():
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--difficulty",
+        "-d",
+        type=str,
+        choices=["hard", "hell", "challenge"],
+        default="hell",
+        help="FinalBoss difficulty (default: hell)",
+    )
+    parser.add_argument(
+        "--clears",
+        type=str,
+        default="20",
+        help="Number of runs to perform (default: 1, set to 'inf' for infinite runs)",
+    )
+    args = parser.parse_args()
+
     FarmingFactory.main_loop(
         farmer=FinalBossFarmer,
         starting_state=States.GOING_TO_FB,  # Should be 'GOING_TO_FB'
-        difficulty="hell",  # Can be "hard", "extreme", "hell", "challenge"
-        num_runs="inf",  # How many runs we want. Set to "inf" for infinite runs.
+        difficulty=args.difficulty,  # Can be "hard", "extreme", "hell", "challenge"
+        num_runs=args.clears,  # How many runs we want. Set to "inf" for infinite runs.
     )
 
 
