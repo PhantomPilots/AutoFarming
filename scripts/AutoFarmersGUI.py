6836d001e4272f1f8a92be82cfaa79bb1c90cca5
PhantomPilot <none>
2025-06-28 14:42:32 -0400

    Removed pytorch requirements and added a main() to the GUI

--- scripts/AutoFarmersGUI.py
+++ scripts/AutoFarmersGUI.py
@@ -430,8 +430,12 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.tabs)
 
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
