1dfe0de4e5aacfbb75624899eec72219a78aa076
PhantomPilot <none>
2025-07-28 19:33:25 -0400

    development update

--- scripts/development.py
+++ scripts/development.py
@@ -35,7 +35,7 @@ def development():
     """Some development function calls"""
     screenshot, window_location = capture_window()
     print("Screenshot shape:", screenshot.shape)
-    screenshot_testing(screenshot, vision_image=vio.cross)
+    # screenshot_testing(screenshot, vision_image=vio.cross)
 
     # if find(vio.connection_confrm_expired, screenshot):
     #     close_game()
