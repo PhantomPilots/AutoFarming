5fd7290ecad2886241b015ebe55e172da1cfad45
PhantomPilot <none>
2025-11-03 06:05:14 -0500

    Play heal only on odd turns

--- scripts/utilities/indura_fighting_strategies.py
+++ scripts/utilities/indura_fighting_strategies.py
@@ -10,6 +10,7 @@ class InduraBattleStrategy(IBattleStrategy):
     """The logic that should pick King's debuff card only if there's a stance present"""
 
     oxidize_count = 0
+    turn_phase3 = 0
 
     def get_next_card_index(
         self, hand_of_cards: list[Card], picked_cards: list[Card], phase: int = 1, card_turn=0, **kwargs
@@ -46,6 +47,7 @@ class InduraBattleStrategy(IBattleStrategy):
         if phase == 1:
             # Reset the oxidize count
             InduraBattleStrategy.oxidize_count = 0
+            InduraBattleStrategy.turn_phase3 = 0
 
             # Disable all heal cards if someone has played one already OR it's the first fight turn!
             if find(vio.mini_heal, six_empty_slots_image) or IBattleStrategy._fight_turn == 0:
@@ -123,17 +125,20 @@ class InduraBattleStrategy(IBattleStrategy):
                     hand_of_cards[idx].card_type = CardTypes.DISABLED
 
         elif phase == 3:
+            InduraBattleStrategy.turn_phase3 += 1
+            print(f"We're on turn {InduraBattleStrategy.turn_phase3}")
+
             if find(vio.mini_heal, six_empty_slots_image):
                 # Disabled all heal cards
                 for idx in heal_card_ids:
                     hand_of_cards[idx].card_type = CardTypes.DISABLED
 
-            # Play a heal if we can
+            # Play a heal if we can, only on odd turns!
             heal_card_ids = sorted(
                 np.where([card.card_type.value == CardTypes.RECOVERY.value for card in hand_of_cards])[0],
                 key=lambda idx: card_ranks[idx],
             )
-            if len(heal_card_ids):
+            if len(heal_card_ids) and InduraBattleStrategy.turn_phase3 % 2 == 1:
                 return heal_card_ids[-1]
 
             # On phase 3, if we have Alpha ult, and haven't played a heal, play a King att card first
