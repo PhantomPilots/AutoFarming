5b1155999269408f1964479e206450cf8f11cd92
PhantomPilot <none>
2025-12-27 08:17:00 +0100

    Minor robustness improvement when finishing dailies

--- scripts/utilities/daily_farming_logic.py
+++ scripts/utilities/daily_farming_logic.py
@@ -113,7 +113,8 @@ class DailyFarmer:
         print("In EXIT FARMER state, trying to exit...")
 
         # First, ensure we're back on the tavern
-        find_and_click(vio.back, screenshot, window_location)
+        if find(vio.back, screenshot, window_location):
+            press_key("esc")
 
         if find(vio.tavern, screenshot) or DailyFarmer.manual_kill:
             if self.complete_callback is not None:
