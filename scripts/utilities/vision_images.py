1366ed644ab4a0454a60b561f572f15b790daac7
PhantomPilot <none>
2026-01-29 06:05:29 -0500

    Fix bug

--- scripts/utilities/vision_images.py
+++ scripts/utilities/vision_images.py
@@ -8,7 +8,7 @@ run_game = MultiVision(
     "run_game_2.png",
     image_name="Run game",
 )
-update_game_ok = Vision("outside\\update_game_ok.png")
+update_game_ok = Vision("outside\\ok_update_game.png")
 password = Vision("password.png")
 sync_code = Vision("sync_code.png")
 server_cancel = MultiVision(
